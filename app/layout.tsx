96fa58cd52f0e1b6c303da0707523251994bc9c5
Yu Hang Mak <yuhangmak123@gmail.com>
2024-07-24 02:39:11 -0400

    Added Google Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "./provider";
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -26,6 +27,7 @@ export default function RootLayout({
         >
           {children}
         </ThemeProvider></body>
+      <GoogleAnalytics gaId="G-EWJBLVX8ZZ" />
     </html>
   );
 }
